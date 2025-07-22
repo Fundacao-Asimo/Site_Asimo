c70e10c6f09069126120ab10b23fe767f6d4f75a
Luiz Augusto <luizaugustosilva871@gmail.com>
2025-07-22 18:32:23 -0300

    Começando logica para mostrar aniversariantes do mes

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,4 +1,18 @@
-export default function MainPage() {
+import ConexaoBD from "../lib/ConexaoBD";
+
+const arquivo = 'usuarios-db.json'
+
+export default async function MainPage() {
+    
+    const mesAtual = new Date().getMonth() + 1;
+    const membros = await ConexaoBD.retornaBD(arquivo);
+
+    const aniversariantes = membros.filter(membro => {
+        if(membro.nasc === "0000-00-00") return false;
+        const mesNascimento = new Date(membro.nasc).getMonth() + 1;
+        return mesNascimento === mesAtual;
+    });
+    
     return(
         <h1>Home</h1>
     );
