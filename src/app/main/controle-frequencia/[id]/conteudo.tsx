a2d62106f8c1d8e4b3af6c7ff9a50d3200b38f5a
luiza <luizaugustosilva871@gmail.com>
2026-04-01 16:38:02 -0300

    Arrumando bug para deploy da segunda versão do site para os membros usarem

--- src/app/main/controle-frequencia/[id]/conteudo.tsx
+++ src/app/main/controle-frequencia/[id]/conteudo.tsx
@@ -1,13 +1,17 @@
 import { EventoProps } from "@/app/_lib/DB_reunioes-eventos";
 import { MembroProps } from "@/app/_lib/DB_user";
+import { redirect } from "next/navigation";
 
 export default function ConteudoControleFrequencia({dadosReuniao, listaMembros}: {dadosReuniao: EventoProps, listaMembros: MembroProps[]})
 {
-
+    if(!dadosReuniao || listaMembros.length <= 0)
+    {
+        redirect("/main");
+    }
 
     return(
         <div>
-            
+            <h1>Trabalhando nisso...</h1>
         </div>
     );
 }
